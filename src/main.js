33bd49ab246f8e06581ea6e0f010d80b1d39c0c1
luguangyao <294984687@qq.com>
2021-07-21 18:59:32 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import { Button } from 'ant-design-vue';
+
+Vue.config.productionTip = false
+Vue.component(Button.name, Button)
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
