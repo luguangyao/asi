12df42ce4e76189df78db58da8217848fc4c82e9
luguangyao <294984687@qq.com>
2021-07-29 19:10:31 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -2,21 +2,33 @@ module.exports = {
   devServer: {
     proxy: {
       '/data/admin': {
-       //  target: 'http://139.159.153.12',
         target: 'http://127.0.0.1/admin',
         changeOrigin: true,
         pathRewrite: {
           '^/data/admin': ''
         }
       },
-      '/data/order': {
-        //  target: 'http://139.159.153.12',
+      '/data/orders': {
          target: 'http://127.0.0.1/order',
          changeOrigin: true,
          pathRewrite: {
-           '^/data/order': ''
+           '^/data/orders': ''
          }
        },
+       '/data/plane': {
+         target: 'http://127.0.0.1/plane',
+         changeOrigin: true,
+         pathRewrite: {
+           '^/data/plane': ''
+         }
+       },
+       '/data/user': {
+        target: 'http://127.0.0.1/user',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/data/user': ''
+        }
+      },
       '/data/location':{
         target:'http://pv.sohu.com/',
         changeOrigin:true,
